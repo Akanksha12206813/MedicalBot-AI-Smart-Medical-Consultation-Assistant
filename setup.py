92b763657b892bb69aef074426ea8e9df7e15dae
akanksha <118536466+Akanksha12206813@users.noreply.github.com>
2026-04-16 11:04:19 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Akanksha Kumari",
+    author_email="akumathur2003@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
